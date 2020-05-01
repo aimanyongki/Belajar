5b843bce4b74d10046bcbff94fbc7c9cc3bdd91a
Yongki Andita Aiman <yongki.aiman@gmail.com>
2020-05-01 23:30:23 +0700

    Update and rename BashTemplate to BashTemplate.sh

---
+++ BashTemplate.sh
@@ -0,0 +1,33 @@
+# A bash template contains the basic components that we usually write on the script.
+# It may contains a Help facility, a licensing statement, a number of simple functions and some logic.
+
+
+#!/bin/bash <br>
+############################################################## 
+#                      scriptTemplate                        # 
+#                                                            # 
+# Use this template as the beginning of a new program. Place # 
+# a short description of the script here.                    # 
+#                                                            # 
+# Change History                                             # 
+# 01/05/2020   Yongki    This is a template for creating new # 
+#                        Bash shell scripts.                 # 
+#                        Add new history entries as needed   # 
+#                                                            # 
+#                                                            #  
+#                                                            # 
+##############################################################
+##############################################################
+############################################################## 
+#                                                            # 
+# Copyright (C) 2007, 2019 David Both                        # 
+# LinuxGeek46@both.org                                       # 
+#                                                            # 
+# This program is free software; you can distribute it and/  # 
+# or modify it under the terms of the GNU General Public     # 
+# License as published by the Free Software Foundation;      # 
+# either version 2 of the License, or (at your opinion) any  # 
+# later version.                                             # 
+#                                                            # 
+#
+#
